07114225d868fa5797ae2ffad25181905afb54a1
konnatoad <severi.moisare@gmail.com>
2024-05-20 11:55:31 +0300

    fixed NSFW warning to be ephemeral

--- src/commands/public/misc/kafka.js
+++ src/commands/public/misc/kafka.js
@@ -10,9 +10,11 @@ module.exports = {
 
   run: async ({ interaction }) => {
     if (!interaction.channel.nsfw)
-      return interaction.reply(
-        "This channel is not NSFW and this command cannot be ran here!"
-      );
+      return interaction.reply({
+        content:
+          "This channel is not NSFW and this command cannot be ran here!",
+        ephemeral: true,
+      });
     let files = fs.readdirSync("src/static/kafka");
     let picture = files[Math.floor(Math.random() * files.length)];
     await interaction.reply({
