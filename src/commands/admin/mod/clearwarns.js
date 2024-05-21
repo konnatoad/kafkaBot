c6f655a8f921752ce8857904b527b66ca627b0c5
konnatoad <severi.moisare@gmail.com>
2024-05-21 10:46:14 +0300

    warning clearing command working

---
+++ src/commands/admin/mod/clearwarns.js
@@ -0,0 +1,56 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  EmbedBuilder,
+} = require("discord.js");
+const warningSchema = require("../../../schemas/warnschema");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clearwarn")
+    .setDescription("This clears a members warnings")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user you want to clear the warnings of")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+  run: async ({ interaction }) => {
+    const { options, guildId, user } = interaction;
+
+    const target = options.getUser("user");
+    const userTag = `${target.username}`;
+
+    const embed = new EmbedBuilder();
+
+    let data = await warningSchema.findOne({
+      GuildID: guildId,
+      UserID: target.id,
+      UserNAME: userTag,
+    });
+
+    if (data) {
+      await warningSchema.findOneAndDelete({
+        GuildID: guildId,
+        UserID: target.id,
+        UserNAME: userTag,
+      });
+
+      embed
+        .setColor("Blurple")
+        .setDescription(
+          `:white_check_mark:   **${target}'s** warnings have been cleared`
+        )
+        .setTimestamp();
+
+      interaction.reply({ embeds: [embed] });
+    } else {
+      interaction.reply({
+        content: `**${target}** has no warnings to be cleared`,
+        ephemeral: true,
+      });
+    }
+  },
+};
