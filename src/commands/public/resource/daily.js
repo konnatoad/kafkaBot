bea5ac27d32611a9e9c61b817d4a410ded7c138c
konnatoad <severi.moisare@gmail.com>
2024-05-21 13:15:39 +0300

    added custom cooldown messages

--- src/commands/public/resource/daily.js
+++ src/commands/public/resource/daily.js
@@ -26,14 +26,21 @@ module.exports = {
         Guild: interaction.guild.id,
       });
 
+      const dailies = [
+        "You have already collected your dailies today. Come back tomorrow.",
+        "Hey don't get too greedy! Come back tomorrow for more.",
+        "You know, they're called dailies for a reason. That means you can only get them once per day. Try again tomorrow!",
+        "Did you forget, you already claimed that today. Come back tomorrow!",
+      ];
+      const dailycooldown = dailies[Math.floor(Math.random() * dailies.length)];
+
       if (userProfile) {
         const lastDailyDate = userProfile.lastDailyCollected?.toString();
         const currentDate = new Date().toLocaleDateString("fi-FI");
 
         if (lastDailyDate === currentDate) {
           interaction.editReply({
-            content:
-              "You have already collected your dailies today. Come back tomorrow.",
+            content: `${dailycooldown}`,
           });
           return;
         }
