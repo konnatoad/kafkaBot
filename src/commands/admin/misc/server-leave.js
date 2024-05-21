0d9bc79af724d186b8a29327d7db999660b1c890
konnatoad <severi.moisare@gmail.com>
2024-05-21 11:45:19 +0300

    added functioning server leave command for the bot. Works with guildId or guild's name

---
+++ src/commands/admin/misc/server-leave.js
@@ -0,0 +1,64 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
+
+module.exports = {
+  devOnly: true,
+  data: new SlashCommandBuilder()
+    .setName("server-leave")
+    .setDescription("Leave a guild")
+    .addStringOption((option) =>
+      option
+        .setName("guild")
+        .setDescription("The ID or the name of the guild to leave")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  run: async ({ interaction, client }) => {
+    const { options } = interaction;
+    const guild = options.getString("guild");
+    await interaction.deferReply({ ephemeral: true });
+
+    async function sendMessage(message) {
+      const embed = new EmbedBuilder()
+        .setColor("Blurple")
+        .setDescription(message);
+
+      await interaction.editReply({ embeds: [embed] });
+    }
+
+    var fetchedGuild = await client.guilds.fetch(guild).catch((err) => {});
+    var guilds = [];
+
+    if (!fetchedGuild) {
+      var gds = await client.guilds.fetch();
+      await gds.forEach(async (value) => {
+        if (value.name == guild) {
+          guilds.push({ name: value.name, id: value.id });
+        }
+      });
+    }
+
+    if (fetchedGuild) {
+      await fetchedGuild.leave();
+      await sendMessage(`I have left ${fetchedGuild.name}`).catch((err) => {});
+    } else {
+      if (guilds.lenght > 1) {
+        await sendMessage(
+          `\`${guild}\` is a name that multiple servers I'm in have! Try using the guild ID to narrow down this search`
+        );
+      } else if (guilds.length == 0) {
+        await sendMessage(`I'm not in a guild matching \`${guild}\``);
+      } else {
+        fetchedGuild = await client.guilds.fetch(guilds[0].id);
+        await fetchedGuild.leave();
+        await sendMessage(`I have left ${fetchedGuild.name}`).catch(
+          (err) => {}
+        );
+      }
+    }
+  },
+};
