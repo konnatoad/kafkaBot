cf59b3815a4dc81b3479a01f73a97ba59c614ef4
konnatoad <severi.moisare@gmail.com>
2024-05-21 13:15:11 +0300

    auto reaction event

---
+++ src/events/messageCreate/reactor.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder } = require("discord.js");
+const reactor = require("../../schemas/reactorschema");
+
+module.exports = async (message) => {
+  const data = await reactor.findOne({
+    Guild: message.guild.id,
+    Channel: message.channel.id,
+  });
+  if (!data) return;
+  else {
+    if (message.author.bot) return;
+    message.react(data.Emoji).catch(async (err) => {
+      const owner = await message.guild.members.cache.get(
+        message.guild.ownerId
+      );
+
+      const embed = new EmbedBuilder()
+        .setColor("Blurple")
+        .setDescription(
+          `Hello there, it looks like i have found an error with the reactor system for your server - **${message.channel}** - and I thought I would bring it to your attention: \`${err}\``
+        );
+      await owner.send({ embeds: [embed] });
+    });
+  }
+};
