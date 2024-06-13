4b462bdbf3499d261ab1fa2f094137f8205a69e3
konnatoad <severi.moisare@gmail.com>
2024-06-13 14:28:46 +0300

    added pagination buttons and user option to list

--- src/commands/admin/configuration/misc/quotecommands.js
+++ src/commands/admin/configuration/misc/quotecommands.js
@@ -1,21 +1,192 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ButtonBuilder,
+} = require("discord.js");
 const Quote = require("../../../../schemas/quoteSchema");
-const { EmbedBuilder } = require("discord.js");
 
-// Function to handle errors
-const handleError = async (interaction, message) => {
-  console.error(message);
-  await interaction.reply({ content: message, ephemeral: true });
+const quotesPerPage = 4;
+
+const createPaginationButtons = (currentPage, totalPages, uniqueId, user) => {
+  const prevButton = new ButtonBuilder()
+    .setCustomId(`prev_page_${uniqueId}`)
+    .setLabel("Previous")
+    .setStyle(ButtonStyle.Primary)
+    .setDisabled(currentPage === 1);
+
+  const nextButton = new ButtonBuilder()
+    .setCustomId(`next_page_${uniqueId}`)
+    .setLabel("Next")
+    .setStyle(ButtonStyle.Primary)
+    .setDisabled(currentPage === totalPages);
+
+  const actionRow = new ActionRowBuilder().addComponents(
+    prevButton,
+    nextButton
+  );
+
+  if (user) {
+    actionRow.addComponents(
+      new ButtonBuilder()
+        .setCustomId(`all_quotes_${uniqueId}`)
+        .setLabel("Show All Quotes")
+        .setStyle(ButtonStyle.Secondary)
+    );
+  }
+
+  return [actionRow];
 };
 
-// Function to add a quote to the database
+const handleError = async (interaction, message) => {
+  console.error(message);
+  try {
+    await interaction.reply({ content: message, ephemeral: true });
+  } catch (error) {
+    console.error("Failed to send error message:", error);
+  }
+};
+
+const replaceMentions = async (content, guild) => {
+  const mentionRegex = /<@!?(\d+)>|<@&(\d+)>|<(a)?:\w+:(\d+)>/g;
+  let match;
+  while ((match = mentionRegex.exec(content))) {
+    if (match[1]) {
+      // User mention
+      const userID = match[1];
+      const member = await guild.members.fetch(userID).catch(() => null);
+      if (member) {
+        content = content.replace(match[0], `@${member.displayName}`);
+      }
+    } else if (match[2]) {
+      // Role mention
+      const roleID = match[2];
+      const role = guild.roles.cache.get(roleID);
+      if (role) {
+        content = content.replace(match[0], `@${role.name}`);
+      }
+    } else if (match[3] && match[4]) {
+      // Emoji mention
+      const animated = match[3] === "a";
+      const emojiId = match[4];
+      const emoji =
+        guild.emojis.cache.get(emojiId) ||
+        (await guild.emojis.fetch(emojiId).catch(() => null));
+      if (emoji) {
+        content = content.replace(match[0], emoji.toString());
+      }
+    }
+  }
+  return content;
+};
+
+const getQuotesPage = async (interaction, page = 1, uniqueId, user) => {
+  try {
+    const query = user
+      ? { guildId: interaction.guildId, authorId: user.id }
+      : { guildId: interaction.guildId };
+    const quotes = await Quote.find(query);
+
+    const totalPages = Math.ceil(quotes.length / quotesPerPage);
+    if (page > totalPages) page = totalPages;
+    const start = (page - 1) * quotesPerPage;
+    const end = start + quotesPerPage;
+    const quotesPage = quotes.slice(start, end);
+
+    let contentPromises = quotesPage.map(async (quote, index) => {
+      let quoteContent = await replaceMentions(
+        quote.content,
+        interaction.guild
+      );
+      quoteContent = quoteContent.replace(/```/g, "\\`\\`\\`"); // Escape triple backticks if necessary
+      const formattedDate = quote.date.toLocaleDateString();
+      return `\`\`\`text\n${start + index + 1}. Quote ID: ${
+        quote._id
+      }\nAuthor: ${
+        quote.author
+      }\nContent: ${quoteContent}\nDate: ${formattedDate}\n\`\`\``;
+    });
+
+    let content = await Promise.all(contentPromises);
+    content = content.join("\n");
+
+    if (quotes.length === 0) {
+      content = user
+        ? `No quotes found for ${user.toString()}.`
+        : "No quotes found.";
+    }
+
+    await interaction.editReply({
+      content: user
+        ? `Quotes for ${user.toString()} (Page ${page}/${totalPages}):\n\n${content}`
+        : `All Quotes (Page ${page}/${totalPages}):\n\n${content}`,
+      components: createPaginationButtons(page, totalPages, uniqueId, user),
+    });
+  } catch (error) {
+    await handleError(
+      interaction,
+      "Failed to load quotes. Please try again later."
+    );
+    console.error("Error fetching quotes:", error);
+  }
+};
+
+const setupCollector = (interaction, uniqueId, user) => {
+  const userId = interaction.user.id;
+  const filter = (i) => i.user.id === userId && i.customId.endsWith(uniqueId);
+
+  const collector = interaction.channel.createMessageComponentCollector({
+    filter,
+    time: 300000, // 5 minutes
+  });
+
+  collector.on("collect", async (buttonInteraction) => {
+    try {
+      const pageMatch =
+        buttonInteraction.message.content.match(/Page (\d+)\/(\d+)/);
+      let currentPage = parseInt(pageMatch[1]);
+      const totalPages = parseInt(pageMatch[2]);
+
+      if (buttonInteraction.customId === `prev_page_${uniqueId}`) {
+        currentPage = Math.max(currentPage - 1, 1);
+      } else if (buttonInteraction.customId === `next_page_${uniqueId}`) {
+        currentPage = Math.min(currentPage + 1, totalPages);
+      } else if (buttonInteraction.customId === `all_quotes_${uniqueId}`) {
+        user = null; // Show all quotes, set user to null
+        await buttonInteraction.deferUpdate(); // Defer update before fetching data
+        await getQuotesPage(interaction, currentPage, uniqueId, user);
+        return; // Exit early to prevent further processing
+      }
+
+      await buttonInteraction.deferUpdate(); // Defer update before fetching data
+      await getQuotesPage(interaction, currentPage, uniqueId, user);
+    } catch (error) {
+      await handleError(
+        buttonInteraction,
+        "Failed to navigate pages. Please try again later."
+      );
+      console.error("Error navigating pages:", error);
+    }
+  });
+
+  collector.on("end", async () => {
+    try {
+      await interaction.editReply({
+        components: [],
+      });
+    } catch (error) {
+      console.error("Error ending collector:", error);
+    }
+  });
+};
+
 const addQuote = async (interaction, messageId) => {
   try {
     const message = await interaction.channel.messages.fetch(messageId);
     const quote = new Quote({
       guildId: interaction.guildId,
       author: message.author.tag,
-      authorId: message.author.id, // Store the author's ID
+      authorId: message.author.id,
       content: message.content,
       date: message.createdAt,
       addedBy: interaction.user.tag,
@@ -34,7 +205,6 @@ const addQuote = async (interaction, messageId) => {
   }
 };
 
-// Function to remove a quote from the database
 const removeQuote = async (interaction, quoteId) => {
   try {
     const result = await Quote.findByIdAndDelete(quoteId);
@@ -50,45 +220,6 @@ const removeQuote = async (interaction, quoteId) => {
   }
 };
 
-// Function to list all quotes as an embed
-const listQuotes = async (interaction) => {
-  try {
-    const quotes = await Quote.find({ guildId: interaction.guildId });
-    const embed = new EmbedBuilder()
-      .setTitle("List of Quotes")
-      .setColor("#0099ff")
-      .setDescription("Here are all the quotes:");
-
-    quotes.forEach((quote, index) => {
-      // Replace <@userID> with actual user mentions
-      let content = quote.content.replace(/<@(\d+)>/g, (match, userID) => {
-        const user = interaction.guild.members.cache.get(userID);
-        return user ? `@${user.displayName}` : match;
-      });
-
-      // Add a field for each quote
-      embed.addFields({
-        name: `Quote ID: ${quote._id}`,
-        value: `**Author:** ${quote.author}\n**Content:** ${content}\n**Added by:** ${quote.addedBy}`,
-      });
-
-      // Add an empty field after each quote except for the last one
-      if (index < quotes.length - 1) {
-        embed.addFields({
-          name: "\u200b", // Zero-width space to create an empty field
-          value: "\u200b", // Zero-width space to create an empty field
-          inline: false,
-        });
-      }
-    });
-
-    await interaction.reply({ embeds: [embed], ephemeral: true });
-  } catch (error) {
-    handleError(interaction, "Failed to list quotes.");
-  }
-};
-
-// Function to remove all quotes from the database
 const removeAllQuotes = async (interaction) => {
   try {
     await Quote.deleteMany({ guildId: interaction.guildId });
@@ -101,45 +232,49 @@ const removeAllQuotes = async (interaction) => {
   }
 };
 
-// Slash command data
-const data = new SlashCommandBuilder()
-  .setName("quote")
-  .setDescription("Manage quotes")
-  .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName("add")
-      .setDescription("Add a quote")
-      .addStringOption((option) =>
-        option
-          .setName("messageid")
-          .setDescription("The ID of the message to quote")
-          .setRequired(true)
-      )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName("remove")
-      .setDescription("Remove a quote")
-      .addStringOption((option) =>
-        option
-          .setName("quoteid")
-          .setDescription("The ID of the quote to remove")
-          .setRequired(true)
-      )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand.setName("list").setDescription("List all quotes")
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName("removeall")
-      .setDescription("Remove all quotes from the server")
-  );
-
-// Export the command data and execution function
 module.exports = {
-  data,
+  deleted: false,
+  data: new SlashCommandBuilder()
+    .setName("quote")
+    .setDescription("Manage quotes")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("add")
+        .setDescription("Add a quote")
+        .addStringOption((option) =>
+          option
+            .setName("messageid")
+            .setDescription("The ID of the message to quote")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("remove")
+        .setDescription("Remove a quote")
+        .addStringOption((option) =>
+          option
+            .setName("quoteid")
+            .setDescription("The ID of the quote to remove")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("list")
+        .setDescription("List all quotes")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("Filter quotes by user")
+            .setRequired(false)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("removeall")
+        .setDescription("Remove all quotes from the server")
+    ),
   async run({ interaction }) {
     try {
       const subcommand = interaction.options.getSubcommand();
@@ -151,7 +286,11 @@ module.exports = {
         const quoteId = interaction.options.getString("quoteid");
         await removeQuote(interaction, quoteId);
       } else if (subcommand === "list") {
-        await listQuotes(interaction);
+        const user = interaction.options.getUser("user");
+        const uniqueId = `${interaction.user.id}-${Date.now()}`;
+        await interaction.deferReply({ ephemeral: true });
+        await getQuotesPage(interaction, 1, uniqueId, user);
+        setupCollector(interaction, uniqueId, user);
       } else if (subcommand === "removeall") {
         await removeAllQuotes(interaction);
       }
