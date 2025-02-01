8eb2f15149a4bccf4b3f8b67ab1aafb36d85f439
konnatoad <severi.moisare@gmail.com>
2025-02-01 04:03:46 +0200

    'update'

--- src/events/ready/console-log.js
+++ src/events/ready/console-log.js
@@ -1,7 +1,6 @@
 module.exports = (client) => {
   console.log(`${client.user.tag} is now online!`);
 
-  // Optional: Debugging log to check if guilds are available
   client.guilds.cache.forEach((guild) => {
     console.log(`Bot is in guild: ${guild.name} (ID: ${guild.id})`);
   });
