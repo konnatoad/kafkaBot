9ec3d2a0804a58d6b7d9a6e7132d8f38e8a11d5d
konnatoad <severi.moisare@gmail.com>
2025-02-17 21:46:10 +0200

    "feat: Add recentQuotes array to dailyQuoteSchema"

--- src/schemas/dailyQuoteSchema.js
+++ src/schemas/dailyQuoteSchema.js
@@ -4,6 +4,7 @@ const dailyQuoteSchema = new mongoose.Schema({
   guildId: { type: String, required: true, unique: true },
   channelId: { type: String, required: true },
   quoteCount: { type: Number, default: 0 },
+  recentQuotes: { type: [String], default: [] }
 });
 
 module.exports = mongoose.model("DailyQuote", dailyQuoteSchema);
